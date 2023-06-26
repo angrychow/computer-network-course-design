c96bf46fb350421c76ec757fae66416dbaffde34
phoenix-cat <91886161+phoenix-cat@users.noreply.github.com>
2023-06-26 22:12:12 +0800

    Update trie.h

--- src/trie.h
+++ src/trie.h
@@ -2,5 +2,12 @@
 #include <stdint.h>
 #include<string.h>
 
-uint32_t trie_search(char * domainName);
+typedef struct Ipset {
+    int num;//ipSet的ip个数
+    uint32_t ip[50 + 5];//该set存储的ip
+} Ipset;
+Ipset ipset[1000 + 5];
+int domainName_num;
+
+int trie_search(char * domainName);
 int trie_insert(char * domainName, uint32_t ipv4);
